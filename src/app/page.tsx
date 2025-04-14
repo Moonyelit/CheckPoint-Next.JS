556833402dce48b57ad1a1276402e752cc3e0e20
moonye@live.fr <moonye@live.fr>
2025-04-14 11:46:31 +0200

    ADD : button WhiteTOBlue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../styles/globals.css'; 
 import Navbar from '@/components/common/Navbar';
+import Button from '@/components/common/Button';
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <p>hello world</p>
+      <Button label="S'inscrire" />
+      
     </>
   );
 }
---
+++ src/components/common/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// Importation du CSS spécifique au bouton
+import './styles/Button.css';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /**
+   * Texte à afficher sur le bouton
+   */
+  label?: string;
+}
+
+export default function Button({ label = "S'inscrire", ...props }: ButtonProps) {
+  return (
+    <button className="btn-custom" {...props}>
+      {label}
+    </button>
+  );
+}
