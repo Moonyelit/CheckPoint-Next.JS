3e26813fa67a13868754fd05a09d58184527f24f
moonye@live.fr <moonye@live.fr>
2025-04-17 15:53:45 +0200

    ADD : NoLogin - Section:NewFunction

--- src/app/home/noLogin/NewFunctionality.tsx
+++ src/app/home/noLogin/NewFunctionality.tsx
@@ -12,8 +12,43 @@ const NewFunctionality = () => {
           className="background-image"
           priority
         />
-        <div className="content">
-          {/* Ajoutez ici le contenu de votre section */}
+        <div className="contentNewFunctionality">
+          <h2 className="Title2-Karantina">Et ce n&apos;est que le début !</h2>
+
+          <div className="Paragraphe1">
+            <p>De nouvelles fonctionnalités sont prévues :</p>
+
+            <ul className="custom-list">
+              <li>
+                <Image
+                  src="/images/Icons/svg/Playstation Icons.svg"
+                  alt="PlayStation Icon"
+                  width={20}
+                  height={20}
+                  className="list-icon"
+                />
+                <span>Importez votre progression via les networks pour suivre vos jeux
+                et statistiques en temps réel.</span>
+              </li>
+              <li>
+                <Image
+                  src="/images/Icons/svg/Playstation Icons.svg"
+                  alt="PlayStation Icon"
+                  width={20}
+                  height={20}
+                  className="list-icon"
+                />
+                <span>Ajoutez vos amis et créez une communauté autour de votre
+                passion.</span>
+              </li>
+            </ul>
+
+            <p>
+              Inscrivez-vous dès maintenant pour gérer votre progression et
+              profiter d’un site en constante évolution, avec de nombreuses
+              nouveautés à venir.
+            </p>
+          </div>
         </div>
       </section>
     </div>
