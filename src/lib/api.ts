9dd71b02e6fdc3a51e961c852f21024dd7b96dc9
moonye@live.fr <moonye@live.fr>
2025-04-18 16:28:40 +0200

    ADD : carrousell

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,8 @@
 import axios from 'axios'
 
-// Création d'une instance axios préconfigurée
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // Défini dans .env.local
-  withCredentials: true, // Nécessaire si tu utilises des cookies pour l'auth
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  // withCredentials: true,
 })
-
-// Tu peux ajouter ici des interceptors plus tard si tu veux gérer les erreurs globales
 
 export default api
