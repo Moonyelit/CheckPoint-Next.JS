f278dc923594b594fccb60302219e407a6571392
TonPseudoExact <fougerouse.elodie@gmail.com>
2025-06-22 00:22:30 +0200

    📁  Réorganisation LegalModal : déplacement du fichier SCSS

--- src/components/common/LegalModal.tsx
+++ src/components/common/LegalModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import './LegalModal.scss';
+import './styles/LegalModal.scss';
 
 interface LegalModalProps {
   isOpen: boolean;
