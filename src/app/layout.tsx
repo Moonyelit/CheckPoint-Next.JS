7b341e72137207a296e871fbbb8cc8650a2632ea
TonPseudoExact <fougerouse.elodie@gmail.com>
2025-06-23 23:38:32 +0200

    ✨ Ajout de préchargement pour les polices et les pages populaires, optimisation des métadonnées et intégration du composant PagePreloader pour améliorer l'expérience utilisateur.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,17 +5,20 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/layout.scss";
 import ConditionalLayout from '@/components/layout/ConditionalLayout';
 import ResourcePreloader from '@/components/common/ResourcePreloader';
+import PagePreloader from '@/components/common/PagePreloader';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
   display: 'swap',
+  preload: true,
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
   display: 'swap',
+  preload: true,
 });
 
 export const metadata: Metadata = {
@@ -24,6 +27,11 @@ export const metadata: Metadata = {
     icon: "/images/Logo/Crystal.png", 
   },
   description: "La plateforme gamifiée pour suivre ta progression et gérer ta collection de jeux vidéo",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=5",
+  robots: "index, follow",
+  other: {
+    "theme-color": "#000000",
+  },
 };
 
 // Ressources critiques à précharger
@@ -44,16 +52,31 @@ export default function RootLayout({
         {/* Préchargement des ressources critiques */}
         <link rel="preload" href="/images/Logo/Crystal.png" as="image" />
         <link rel="dns-prefetch" href="//images.igdb.com" />
-        <link rel="preconnect" href="//images.igdb.com" />
+        <link rel="preconnect" href="//images.igdb.com" crossOrigin="anonymous" />
+        
+        {/* Préchargement des polices */}
+        <link rel="preload" href="/_next/static/media/geist-sans-var.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/_next/static/media/geist-mono-var.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        
+        {/* Préchargement des pages populaires */}
+        <link rel="prefetch" href="/search" />
+        <link rel="prefetch" href="/connexion" />
+        <link rel="prefetch" href="/inscription" />
+        
+        {/* Optimisations de performance */}
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="format-detection" content="telephone=no" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
         suppressHydrationWarning={true}
       >
         <ResourcePreloader resources={criticalResources} />
-        <ConditionalLayout>
-          {children}
-        </ConditionalLayout>
+        <PagePreloader>
+          <ConditionalLayout>
+            {children}
+          </ConditionalLayout>
+        </PagePreloader>
       </body>
     </html>
   );
