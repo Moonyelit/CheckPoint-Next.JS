3a9e5f2430ff65f143c9430765a62b7f1e7bf00c
Moonye <fougerouse.elodie@gmail.com>
2025-07-16 10:33:52 +0200

    ✨ Amélioration du composant Button : ajout de la gestion de l'accessibilité avec ariaDescription et mise à jour de la documentation des props.

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -5,15 +5,45 @@ import './styles/Button.scss';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   /**
+   * Cette interface étend React.ButtonHTMLAttributes<HTMLButtonElement>
+   * ce qui signifie qu'elle hérite automatiquement de TOUS les attributs HTML
+   * d'un élément <button> natif :
+   * - onClick, onKeyDown, onFocus, etc. (événements)
+   * - disabled, type, id, className, etc. (attributs HTML)
+   * - aria-label, aria-describedby, etc. (accessibilité)
+   * - style, data-*, etc. (autres attributs)
+   * 
+   * Cela permet d'utiliser ce composant comme un bouton HTML normal
+   * tout en ajoutant nos propriétés personnalisées (label, ariaDescription)
+   */
+  /**
    * Texte à afficher sur le bouton
    */
   label?: string;
+  /**
+   * Description pour les lecteurs d'écran
+   */
+  ariaDescription?: string;
 }
 
-export default function Button({ label = "S'inscrire", ...props }: ButtonProps) {
+export default function Button({ 
+  label = "S'inscrire", 
+  ariaDescription,
+  ...props 
+}: ButtonProps) {
+  
   return (
-    <button className="btn-custom" {...props}>
+    <button 
+      className="btn-custom" 
+      aria-label={label}
+      {...props}
+    >
       {label}
+      {ariaDescription && (
+        <span className="sr-only">
+          {ariaDescription}
+        </span>
+      )}
     </button>
   );
 }
